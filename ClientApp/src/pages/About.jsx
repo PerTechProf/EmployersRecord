3a699e86e653690e970910fe95fafbef7693ae3f
Alex <gamesevolutionteam@gmail.com>
2022-06-06 07:19:03 +0300

    feat: No about when logged in

--- ClientApp/src/pages/About.jsx
+++ ClientApp/src/pages/About.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
+import { useToken } from '../logic/hooks'
 
 export const About = () => {
-  return <div style={{ width: "90%", margin: "0 auto", marginTop: "2%" }}>
+  const token = useToken();
+
+  return !token ? <div style={{ width: "90%", margin: "0 auto", marginTop: "2%" }}>
           <h2>Веб-приложение для учёта кадров предприятия Tinfoil</h2>
           <span> Для начала работы с приложением требуется <Link to="login">войти</Link> в систему</span>
-        </div>
+        </div> : <Navigate to="applications" replace />
 }
